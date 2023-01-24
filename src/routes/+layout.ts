b56ce4ee2629bcff3bb5e899f8ae2193c8a41c2d
sheeyang <chewsheeyang01@gmail.com>
2023-01-25 01:34:10 +0800

    Disable all console output in production

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,12 @@
+import { dev } from '$app/environment';
+
 export const ssr = false;
 export const csr = true;
+
+// Disable all console output in production
+if (!dev) {
+	for (const key in console) {
+		/*** @ts-expect-error: ***/
+		console[key] = () => void 0;
+	}
+}
